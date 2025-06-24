66ca0ef6c3fb657eb725046f4c9d78c74a97b646
Sokhibjon <soxib.davurov@gmail.com>
2025-06-24 23:33:44 +0900

    feat: members service integrations

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from "express";
 import {T} from "../libs/types/common"
+import {} from '../models/Member.service'
 
 const restaurantController: T= {};
 restaurantController.goHome = (req: Request, res: Response) => {

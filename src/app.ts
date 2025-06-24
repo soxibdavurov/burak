5c3276527d71a3518f1c08e36e46f03d49418960
Sokhibjon <soxib.davurov@gmail.com>
2025-06-24 23:10:39 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import path from "path";
+import router from "./router";
 
 /** I Entrance */
 const app = express();
 console.log("__dirname", __dirname);
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(express.urlencoded({extended: true})); // Middleware DP -> Traditional API
+app.use(express.json()); //Middleware DP -> RestAPI
 
 /** II Sessions */
 
@@ -15,5 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 /** IV Routers */
+app.use("/", router);  // Middleware Design Pattern
+
 
 export default app; //module.exports
--- src/train.ts
+++ src/train.ts
@@ -176,7 +176,15 @@ shop.qoldiq();
 
 // countLetters(mixWord);
 
-/*                  TASK A              */
+/*                  TASK A              
+Harf sifatida kiritilgan birinchi parametr, 
+kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
+Funktsiya tuzing
+
+Masalan: countLetter("e", "engineer")
+'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
+3 sonini qaytaradi
+*/
 
 // let word = "mexanizatsiyalshtirilgandanmisizlar"; //Sanash kerak bo'lgan so'z
 // let letter = "a";

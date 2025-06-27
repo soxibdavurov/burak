6b75c708d59a84c21403d6762e20779c12bb5203
soxibdavurov <soxib.davurov@gmail.com>
2025-06-27 23:08:44 +0900

    develop signup

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,6 +1,28 @@
+import MemberModel from "../schema/Member.model";
+import { Member, MemberInput } from "../libs/types/member";
+import Errors, {HttpCode, Message} from "../libs/Errors";
+import { MemberType } from "../libs/enums/member.enum";
 class MemberService {
-    constructor () {
+    private readonly memberModel;
 
+    constructor () {
+        this.memberModel = MemberModel;
+    }
+
+    public async processSignup(input: MemberInput): Promise<Member>{
+       const exist = await this.memberModel
+       .findOne({memberType: MemberType.RESTAURANT}).
+       exec();
+console.log("exist", exist);
+
+    if(exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CR_FAIL);   
+        try {
+        const result = await this.memberModel.create(input);
+       result.memberPassword = "";
+       return result;
+       }catch (err) {
+        throw new Errors(HttpCode.BAD_REQUEST, Message.CR_FAIL);
+       }
     }
 }
 

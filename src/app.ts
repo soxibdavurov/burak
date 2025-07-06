7724e336137576a5712c542d1d851d94cc0d7e37
soxibdavurov <soxib.davurov@gmail.com>
2025-07-06 09:24:20 +0900

    feat: inegrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,15 @@ import routerAdmin from "./router-admin"
 import morgan from 'morgan';
 import { MORGAN_FORMAT } from './libs/config';
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore =ConnectMongoDB(session);
+const store = new MongoDBStore({
+uri: String(process.env.MONGO_URL),
+collection: "sessions",
+});
+
 /** I Entrance */
 const app = express();
 console.log("__dirname", __dirname);
@@ -14,6 +23,18 @@ app.use(express.json()); //Middleware DP -> RestAPI
 app.use(morgan(MORGAN_FORMAT));
 
 /** II Sessions */
+app.use(
+    session({
+        secret: String(process.env.SESSION_SECRET),
+        cookie: {
+        maxAge: 1000 * 3600 * 3 // 3h
+  },
+  store: store,
+  resave: false, // 10:30 auth => 13:30 12:00 => 15:00
+  saveUninitialized: true   
+    })
+);
+
 
 /** III Views */
 app.set('views', path.join(__dirname, 'views'));

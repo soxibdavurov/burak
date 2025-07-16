dee8db8455b76afce513039f66b5a40d76142e5e
soxibdavurov <soxib.davurov@gmail.com>
2025-07-17 02:16:51 +0900

    Task O done

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,29 @@
 console.log("Signup frontend javascript file");
 
 $(function () {
+    const fileTarget = $(".file-box .upload-hidden");
+    let filename;
+    
+    fileTarget.on("change", function () {
+        if(window.FileReader) {
+            const uploadFile = $(this) [0].files[0];
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+            if(!validImageType.includes(fileType)) {
+                alert("Please insert only jpeg, jpg or png!");
+            } else {
+                if(uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame")
+                    .attr("src", URL.createObjectURL(uploadFile))
+                    .addClass("sucess");
+                }
+                filename = $(this)[0].files[0].name;
+            }
+
+            $(this).siblings(".upload-name").val(filename);
+        }
+    })
 });
 
 function validateSignupForm(){
@@ -25,5 +48,14 @@ function validateSignupForm(){
         
         return false;
 
+    }
+
+    const memberImage = $(".member-image").get(0).files[0].name
+    ? $(".member-image").get(0).files[0].name
+    : null;
+
+    if(!memberImage) {
+        alert("Please insert restaurant image!");
+        return false;
     }
 }

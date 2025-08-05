b58bcd58b6c912de7e940bc2320d2ef6a7c93164
soxibdavurov <soxib.davurov@gmail.com>
2025-08-05 10:19:01 +0900

    Task W done

--- src/train.ts
+++ src/train.ts
@@ -38,6 +38,34 @@ Pipe validation
 Database
  */
 
+/*
+TASK W
+
+Shunday function yozing, u o'ziga parametr sifatida
+yagona array va number qabul qilsin. Siz tuzgan function
+arrayni numberda berilgan uzunlikda kesib bo'laklarga
+ajratgan holatida qaytarsin.
+MASALAN: chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3);
+return [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]];
+
+Yuqoridagi namunada berilayotgan array ikkinchi parametr 3'ga
+asoslanib 3 bo'lakga bo'linib qaytmoqda. Qolgani esa o'z holati qolyapti
+*/
+
+function chunkArray<T>(array: T[], size: number): T[][] {
+  const result: T[][] = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+
+  return result;
+}
+
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const chunks = chunkArray(input, 3);
+console.log(chunks);
+
 /* TASK V
 
 Shunday function yozing, uni string parametri bo'lsin.

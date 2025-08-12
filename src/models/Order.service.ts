9a5624d17fa7b875dda9076cc38beb2e88f61435
soxibdavurov <soxib.davurov@gmail.com>
2025-08-13 01:33:45 +0900

    feat: develop getMyOrders res Api

--- src/models/Order.service.ts
+++ src/models/Order.service.ts
@@ -1,11 +1,12 @@
 import { Member } from "../libs/types/member";
 import OrderModel from "../schema/Order.model";
 import OrderItemModel from "../schema/OrderItem.model";
-import { Order, OrderItemInput } from "../libs/types/order";
+import { Order, OrderInquiry, OrderItemInput } from "../libs/types/order";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 import Errors, { Message } from "../libs/Errors";
 import { HttpCode } from "../libs/Errors";
 import {ObjectId} from "mongoose";
+import { OrderStatus } from "../libs/enums/order.enum";
 
 class OrderService {
     private readonly orderModel;
@@ -56,6 +57,42 @@ class OrderService {
 
         const orderItemsState = await Promise.all(promisedList);
         console.log("orderItemState:", orderItemsState);
+    }
+
+public async getMyOrders(
+    member:Member,
+    inquiry: OrderInquiry
+): Promise<Order[]> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+    const matches = {memberId: memberId, orderStatus: inquiry.orderStatus};
+
+    const result = await this.orderModel.aggregate([
+        {$match: matches},
+        {$sort: {updatedAt: -1}},
+        {$skip: (inquiry.page -1) * inquiry.limit},
+        {$limit: inquiry.limit},
+        {
+            $lookup: {
+                from: "orderItems",
+                localField: "_id",
+                foreignField: "orderId",
+                as: "orderItems",
+            },
+        },
+        {
+            $lookup: {
+                from: "products",
+                localField: "orderItems.productId",
+                foreignField: "_id",
+                as: "productData"
+            },
+        }
+    ])
+    .exec();
+
+    if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.N_D_F);
+
+    return result;
     }
 }
 
--- src/router.ts
+++ src/router.ts
@@ -39,4 +39,10 @@ router.post("/order/create",
     orderController.createOrder
 );
 
+router.get(
+    "/order/all", 
+    memberController.verifyAuth,
+    orderController.getMyOrders
+);
+
 export default router;

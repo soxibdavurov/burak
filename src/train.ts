48fcb8946608744d3344d2841c9b0ebcdc7175e6
soxibdavurov <soxib.davurov@gmail.com>
2025-08-20 22:49:11 +0900

    Task ZC done

--- src/train.ts
+++ src/train.ts
@@ -38,6 +38,34 @@ Pipe validation
 Database
  */
 
+/* TASK ZC
+
+Selisy (°C) shkalasi bo'yicha raqam qabul qilib, uni
+Ferenhayt (°F) shkalisaga o'zgaritib beradigan function yozing.
+
+MASALAN: celsiusToFahrenheit(0) return 32;
+MASALAN: celsiusToFahrenheit(10) return 50;
+
+Yuqoridagi misolda, 0°C, 32°F'ga teng.
+Yoki 10 gradus Selsiy, 50 Farenhaytga teng.
+
+°C va °F => Tempraturani o'lchashda ishlatiladigan o'lchov birligi.
+
+*/
+
+function celsiusToFahrenheit(celsius: number): number {
+  return (celsius * 9/5) + 32;
+}
+function fahrenheitToCelsius(fahrenheit: number): number {
+  return (fahrenheit - 32) * 5/9;
+}
+
+
+console.log(celsiusToFahrenheit(0));   
+console.log(celsiusToFahrenheit(10));  
+console.log(fahrenheitToCelsius(32));  
+console.log(fahrenheitToCelsius(50)); 
+
 /* 
 TASK-ZB:
 
@@ -46,11 +74,11 @@ Shunday function yozing, uni 2 ta number parametri bolsin va berilgan sonlar ora
 MASALAN: randomBetween(30, 50) return 45
 */
 
-function randomBetween(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// function randomBetween(min: number, max: number): number {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
-console.log(randomBetween(30, 50)); 
+// console.log(randomBetween(30, 50)); 
 
 /* 
 TASK Z

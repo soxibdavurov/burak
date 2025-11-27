15792209bd387dcd22c656dd52c2ba0de7123ae1
soxibdavurov <soxib.davurov@gmail.com>
2025-11-28 01:42:31 +0900

    feat: prepare for production

--- dist/server.js
+++ dist/server.js
@@ -3,9 +3,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Executed!");
-const moment_1 = __importDefault(require("moment"));
-const currentTime = (0, moment_1.default)().format("YYYY MM DD");
-console.log(currentTime);
-const person = "Martin";
-const count = 100;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({
+    path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+});
+const mongoose_1 = __importDefault(require("mongoose"));
+const app_1 = __importDefault(require("./app"));
+mongoose_1.default.connect(process.env.MONGO_URL, {})
+    .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    app_1.default.listen(PORT, function () {
+        console.log(`The server is running successfully on port: ${PORT}`);
+        console.info(`Admin project on http://localhost:${PORT}/admin \n`);
+    });
+})
+    .catch((err) => console.log("ERROR on connection MongoDB", err));

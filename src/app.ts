9db919f3c669776515151036109a32f61775cb79
soxibdavurov <soxib.davurov@gmail.com>
2025-11-28 00:36:20 +0900

    feat: develop socket.io server

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,17 @@ import routerAdmin from "./router-admin"
 import morgan from 'morgan';
 import { MORGAN_FORMAT } from './libs/config';
 import cookieParser from "cookie-parser";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
+
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
 import { T } from './libs/types/common';
 
 const MongoDBStore = ConnectMongoDB(session);
 const store = new MongoDBStore({
-uri: String(process.env.MONGO_URL),
-collection: "sessions",
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
 });
 
 /** I Entrance */
@@ -21,13 +24,13 @@ const app = express();
 console.log("__dirname", __dirname);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("./uploads"));
-app.use(express.urlencoded({extended: true})); // Middleware DP -> Traditional API
+app.use(express.urlencoded({ extended: true })); // Middleware DP -> Traditional API
 app.use(express.json()); //Middleware DP -> RestAPI
 app.use(
   cors({
-  credentials: true,
-  origin: true,
-})
+    credentials: true,
+    origin: true,
+  })
 );
 app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
@@ -35,17 +38,17 @@ app.use(morgan(MORGAN_FORMAT));
 /** II Sessions */
 app.use(
   session({
-        secret: String(process.env.SESSION_SECRET),
-        cookie: {
-        maxAge: 1000 * 3600 * 3 // 3h
-  },
-  store: store,
-  resave: true, // 10:30 auth => 13:30 | 12:00 => 15:00
-  saveUninitialized: true   
-    })
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3 // 3h
+    },
+    store: store,
+    resave: true, // 10:30 auth => 13:30 | 12:00 => 15:00
+    saveUninitialized: true
+  })
 );
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const sessionInstance = req.session as T;
   res.locals.member = sessionInstance.member;
   next()
@@ -61,4 +64,23 @@ app.set("view engine", "ejs");
 app.use("/admin", routerAdmin);  // Middleware Design Pattern EJS  SSR
 app.use("/", router);  // Middleware Design Pattern: React  Frontend
 
-export default app; //module.exports
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
+
+let summaryClient = 0;
+io.on("connection", (socket) => {
+  summaryClient++;
+  console.log(`Connection & total [${summaryClient}]`);
+
+  socket.on("disconnect", () => {
+    summaryClient--;
+    console.log(`Disconnection & total [${summaryClient}]`);
+  })
+});
+
+export default server; //module.exports
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
-import app from "./app";
+import server from "./app";
 
 
 mongoose.connect(process.env.MONGO_URL as string, {})
-.then((data) =>{
-    console.log("MongoDB connection succeed");
-    const PORT = process.env.PORT ?? 3003;
-    app.listen(PORT, function () {
-        console.log(`The server is running successfully on port: ${PORT}`);
-        console.info(`Admin project on http://localhost:${PORT}/admin \n`);
+    .then((data) => {
+        console.log("MongoDB connection succeed");
+        const PORT = process.env.PORT ?? 3003;
+        server.listen(PORT, function () {
+            console.log(`The server is running successfully on port: ${PORT}`);
+            console.info(`Admin project on http://localhost:${PORT}/admin \n`);
+        })
     })
-})
-.catch((err) =>console.log("ERROR on connection MongoDB",err));
+    .catch((err) => console.log("ERROR on connection MongoDB", err));
